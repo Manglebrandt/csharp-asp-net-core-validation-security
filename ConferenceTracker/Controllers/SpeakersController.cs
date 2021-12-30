668579b3f1b782725de6b3fe600c7007b895546e
Mike Anglebrandt <manglebrandt@epfc.com>
2021-12-30 16:02:40 -0500

    add validation stuff?

--- ConferenceTracker/Controllers/SpeakersController.cs
+++ ConferenceTracker/Controllers/SpeakersController.cs
@@ -46,10 +46,14 @@ namespace ConferenceTracker.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Administrators")]
-        public IActionResult Create(Speaker speaker)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Speaker speaker)
         {
-            _speakerRepository.Create(speaker);
-            return RedirectToAction(nameof(Index));
+	        if (!ModelState.IsValid) return View(speaker);
+	        _speakerRepository.Create(speaker);
+
+	        return RedirectToAction(nameof(Index));
+
         }
 
         [HttpGet]
